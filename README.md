a6faec49c218514e779a14a56bbe29c30d2cc0db
fitzterra <github_subs@icave.net>
2019-03-29 09:17:59 +0200

    The installer can now do basic installs and cleanups

--- README.md
+++ README.md
@@ -7,6 +7,22 @@ Some really good resource: https://github.com/webpro/awesome-dotfiles
 
 This will fianlly replace my easyEnv project started years ago: https://github.com/fitzterra/easyEnv
 
+Assumptions
+-----------
+
+The following assumptions are made to make things easier to get this going, but
+will not be very cross platform if not given attention:
+
+* A Debian based environment is assumed
+* A bash shell is used
+* For this environment, setting up the bash shell adds a hook into `~/.bashrc`
+    which then sources any local bash mods. This leaves `~/.bashrc` as close as
+    possible to the maintainers version and makes it easy to add updated basrc
+    version if needed.
+* We use `xstow` instead of `stow` (no particular reason, other than it seems to
+    add additional features which may be useful), and because this Debian, we
+    offer to install it using apt-get if not installed
+* For installing via apt-get, we assume you have sudo powers
 
 Usage
 -----
--- install.sh
+++ install.sh
@@ -4,6 +4,11 @@
 
 ##~~ Variables ~~##
 MYDIR=$(dirname $0)
+ME=$(basename $0)
+# Declare an associative array where the keys are the component names, and the
+# values are info strings about the component retrieved from the
+# README.component files in their respective dirs.
+declare -A COMPS
 
 ##~~ Functions ~~##
 function setBashRcHook ()
@@ -73,5 +78,156 @@ __HOOK__
 	done
 }
 
-HOOKFILES=hooktest.txt
-setBashRcHook $@
+# Check that xstow is installed, and if not ask to install it
+function checkXstow () {
+    STOWER=xstow
+
+    if (which $STOWER >/dev/null); then
+        return
+    fi
+
+    read -p "The '$STOWER' command is required. Install it now? [Y/n]: " -n 1 ans
+    # Add a newline to the output only if enter was not pressed
+    [ "$ans" != "" ] && echo
+    # Set default answer to yes if enter was pressed and translate to lower case
+    ans=$(echo ${ans:-y} | tr 'A-Z' 'a-z')
+
+    # We can not continue if we can not install xstow
+    if [ "$ans" = "n" ]; then
+        echo "Using this dotfiles manager requires $STOWER. Can not continue, sorry."
+        exit 1
+    fi
+
+    echo -e "Attempting to install $STOWER...\n"
+    sudo apt-get install $STOWER
+
+    if [ $? -ne 0 ]; then
+        echo -e "\nCan not continue until this is fixed. Sorry..."
+        exit 1
+    fi
+}
+
+function usage() {
+    cat << _EOU_
+
+Installs or cleans up, all or only certain, dotfile components.
+
+Usage: $ME [clean] [-c component] [-h]
+
+commands:
+    clean   Will clean up instead of install
+
+options:
+    -c comp,comp... Comma seperated list of components to install or clean. If
+                    not supplied, all components are selected. Use -c list to
+                    get a list of available components.
+    -h              Show this help
+    
+_EOU_
+}
+
+##--
+# Determines all available components for installation and sets the global
+# COMPS array
+##--
+function getComponents() {
+    # Components are all dirs containing a README.component file
+    for c in $(find . -maxdepth 2 -name README.component); do
+        comp=$(dirname $c | sed 's@^\./@@')
+        COMPS[$comp]=$(cat $c)
+    done
+}
+
+##--
+# Show a list of all available components and their short description
+##--
+function showComponents() {
+    echo -e "\nAvailable components:\n"
+    for c in ${!COMPS[@]}; do
+        printf "%-10.10s : %s\n" $c "${COMPS[$c]}"
+    done
+    echo
+}
+
+##--
+# Does an install - components to install are passed as arguments
+##-
+function install() {
+    for c in $@; do
+        case "$c" in
+            bash)
+                # We need to also do the bashrc hook
+                setBashRcHook
+                xstow -v -i README.component -t ~/ $c || exit 1
+                ;;
+            *)
+                # Anything else is just a stow away
+                xstow -v -i README.component -t ~/ $c || exit 1
+        esac
+    done
+}
+
+##--
+# Does a cleanup - components to install are passed as arguments
+##-
+function cleanup() {
+    for c in $@; do
+        case "$c" in
+            bash)
+                # We need to also do the bashrc hook
+                setBashRcHook -d
+                xstow -v -D -i README.component -t ~/ $c || exit 1
+                ;;
+            *)
+                # Anything else is just a stow away
+                xstow -v -D -i README.component -t ~/ $c || exit 1
+        esac
+    done
+}
+
+# Get all available components
+getComponents
+
+# Quick and dirty command line args parsing - starting off with an empty
+# component list
+while [ "$1" != "" ]; do
+    case "$1" in
+        -h) usage && exit 0
+            ;;
+        -c) shift
+            [ "$1" = "list" ] && showComponents && exit 0
+            # Validate each component
+            for c in $(echo "$1" | tr ',' '\n'); do
+                # Check if it is defined in the available components list
+                if [[ ! -v "COMPS[$c]" ]]; then
+                    echo "Component [$c] is not a valid component. Try -h"
+                    exit 1
+                fi
+                # TODO: Should check if already there and not add again
+                COMPLIST="$COMPLIST $c"
+            done
+            ;;
+        clean)
+            CLEANUP='1'
+            ;;
+        *) echo "Invalid argument: '$1'. Try -h" && exit 1
+            ;;
+    esac
+    shift
+done
+
+# Use all components if none were specified
+COMPLIST=${COMPLIST:-"${!COMPS[*]}"}
+
+# I have to be run from my install dir
+cd $MYDIR
+
+# We need xstow
+checkXstow
+
+# Do the work
+if [ "$CLEANUP" = "" ]; then
+    install $COMPLIST
+else
+    cleanup $COMPLIST
+fi
